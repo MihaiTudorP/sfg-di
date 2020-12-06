510f5fb69c76ca1f5106e6a6de476043e1fccac0
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-12-06 16:59:34 +0200

    Demo Spring default profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
@@ -3,11 +3,11 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hola muchachos! Estoy embarasada! :D - ES";
+        return "¡Hola muchachos! Estoy embarasada! :D - ES";
     }
 }
